129e4f5b588ad1bd4cd02e19287cc1e12bd95c84
ec23b1044-oss <ec23b1044@iiitdm.ac.in>
2026-04-15 16:50:59 +0530

    Update get-history.js

--- api/get-history.js
+++ api/get-history.js
@@ -1,19 +1,6 @@
 export default async function handler(req, res) {
 const { device_id, password } = req.body;
 
-const device = await axios.get(
-  `${SUPABASE_URL}/rest/v1/devices?device_id=eq.${device_id}&device_password=eq.${password}`,
-  {
-    headers: {
-        apikey: process.env.SUPABASE_SERVICE_ROLE_KEY,
-        Authorization: `Bearer ${process.env.SUPABASE_SERVICE_ROLE_KEY}`
-    }
-  }
-);
-
-if (!device.data.length) {
-  return res.status(401).send("Unauthorized");
-}
   
 
   const r = await fetch(
