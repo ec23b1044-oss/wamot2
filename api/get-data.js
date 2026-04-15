57cd4ece19edf1cd980d65ef646541e7bdc38250
ec23b1044-oss <ec23b1044@iiitdm.ac.in>
2026-04-15 16:49:49 +0530

    Update get-data.js

--- api/get-data.js
+++ api/get-data.js
@@ -22,8 +22,8 @@ export default async function handler(req, res) {
   `${SUPABASE_URL}/rest/v1/devices?device_id=eq.${device_id}&device_password=eq.${password}`,
   {
     headers: {
-      apikey: SERVICE_KEY,
-      Authorization: `Bearer ${SERVICE_KEY}`
+        apikey: process.env.SUPABASE_SERVICE_ROLE_KEY,
+        Authorization: `Bearer ${process.env.SUPABASE_SERVICE_ROLE_KEY}`
     }
   }
 );
