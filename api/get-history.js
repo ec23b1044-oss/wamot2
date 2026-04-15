502ce55c137be340538567864e26969688612f79
ec23b1044-oss <ec23b1044@iiitdm.ac.in>
2026-04-15 16:24:34 +0530

    Update get-history.js

--- api/get-history.js
+++ api/get-history.js
@@ -1,16 +1,5 @@
 export default async function handler(req, res) {
-const { device_id, password } = req.body;
 
-// verify
-const { data: user } = await supabase
-  .from("devices")
-  .select("*")
-  .eq("device_id", device_id)
-  .eq("device_password", password);
-
-if (!user || user.length === 0)
-  return res.status(401).json({ error: "Unauthorized" });
-  
   const { device_id } = req.body;
 
   const r = await fetch(
